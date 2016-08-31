6b581bf42435b37fec0c59e98f39273f66baa38f
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2016-08-31 22:10:55 +1000

    made endpoints dynamic
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- page/010-constructor.js
+++ page/010-constructor.js
@@ -24,7 +24,8 @@ var Page = (function() {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 		DEBUG: [Debug],   //Enable/disable debugger
 		DEBUGfilter: [],  //filter debug messages
-		ENDPOINTS: 'http://localhost:1338/status/', //Where the server sits
+		[debugcomment]ENDPOINTS: 'http://localhost:1338/status/', //Where the server sits
+		[prodcomment]ENDPOINTS: 'https://gel.westpacgroup.com.au:8081/status/', //Where the server sits
 		TIMEFRAME: [      //allowed keywords for time frame
 			'day',
 			'week',
