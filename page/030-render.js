ba5ca672cb9d8b9c9df5e2617fac80fb9bc5d70d
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2016-09-01 07:46:30 +1000

    added comment
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- page/030-render.js
+++ page/030-render.js
@@ -58,7 +58,7 @@
 					titlePosition: 'none', //we certainly don’t want to show it
 					colors: ['#42a5f5'], //the line color
 					backgroundColor: '#263238', //background color duh!
-					lineWidth: 1,
+					lineWidth: 1,  //looks nicer with the amount of data
 					hAxis: {
 						slantedText: false, //nah that looks weird
 						maxAlternation: 1, //no alternating multiple lines
